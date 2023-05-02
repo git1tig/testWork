2740ca871aede7359b76c26243ac726d89e4f1a5
ALexSer <5440918@gmail.com>
2023-05-02 20:25:06 +0300

    Метод подсчёта количества коротких строчек готов

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int GetCountByLength(string [] myarray, int lengthLimit)
+{
+    int count=0;
+    int i=0;
+    while (i<myarray.Length)
+    {
+        if (myarray[i].Length <= lengthLimit) count++;
+    }
+    
+  return count;  
+}
+
+
